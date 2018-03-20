9adcd847f189aa3a776013f3c087ccc8fd498140
samalie <32448429+samalie@users.noreply.github.com>
2018-03-20 11:01:07 +0100

    Button (JS)

--- js/js.js
+++ js/js.js
@@ -6,3 +6,11 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+  $( function() {
+    $( ".widget input[type=submit], .widget a, .widget button" ).button();
+    $( "button, input, a" ).click( function( event ) {
+      event.preventDefault();
+    } );
+  } );
